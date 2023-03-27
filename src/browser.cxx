db781b4fc4971d34f63e799026287b9ef8441d32
Adam <classygopher@gmail.com>
2023-03-27 14:50:56 +0100

    browser: set overlay correctly

--- src/browser.cxx
+++ src/browser.cxx
@@ -4,12 +4,13 @@
 
 Browser::Window::Window(CefRefPtr<CefClient> client, Browser::Details details) {
 	CefBrowserSettings browser_settings;
+	browser_settings.background_color = CefColorSetARGB(0xFF, 0xFF, 0xFF, 0xFF);
 	CefRefPtr<CefBrowserViewDelegate> bvd = new Browser::BrowserViewDelegate(details);
 	CefRefPtr<CefBrowserView> browser_view = CefBrowserView::CreateBrowserView(client, "https://adamcake.com", browser_settings, nullptr, nullptr, bvd);
 	CefRefPtr<CefBrowserView> controls_overlay = nullptr;
 	if (details.controls_overlay) {
-		CefRefPtr<CefBrowserViewDelegate> bvd = new Browser::BrowserViewDelegate(details); // TODO: is this line necessary?
-		controls_overlay = CefBrowserView::CreateBrowserView(client, "", browser_settings, nullptr, nullptr, bvd);
+		browser_settings.background_color = CefColorSetARGB(0, 0, 0, 0);
+		controls_overlay = CefBrowserView::CreateBrowserView(client, "http://bolt/app-overlay", browser_settings, nullptr, nullptr, bvd);
 	}
 	this->window_delegate = new Browser::WindowDelegate(std::move(browser_view), std::move(bvd), std::move(controls_overlay), details);
 	CefWindow::CreateTopLevelWindow(this->window_delegate);
