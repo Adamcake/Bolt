bef31748294539a099de873a5b30a851d469975b
Adam <classygopher@gmail.com>
2023-03-30 18:54:43 +0100

    dom_visitor: null check app-frame

--- src/browser/dom_visitor.cxx
+++ src/browser/dom_visitor.cxx
@@ -5,5 +5,8 @@ Browser::DOMVisitorAppFrame::DOMVisitorAppFrame(const CefString app_url): app_ur
 }
 
 void Browser::DOMVisitorAppFrame::Visit(CefRefPtr<CefDOMDocument> document) {
-	document->GetElementById("app-frame")->SetElementAttribute("src", this->app_url);
+	CefRefPtr<CefDOMNode> app_frame = document->GetElementById("app-frame");
+	if(app_frame) {
+		app_frame->SetElementAttribute("src", this->app_url);
+	}
 }
