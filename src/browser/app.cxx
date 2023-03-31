a8711d79002d67120c147848e9eef0dfd646247a
Adam <classygopher@gmail.com>
2023-03-31 17:55:37 +0100

    browser/app: debug prints

--- src/browser/app.cxx
+++ src/browser/app.cxx
@@ -18,6 +18,7 @@ CefRefPtr<CefLoadHandler> Browser::App::GetLoadHandler() {
 }
 
 void Browser::App::OnBrowserCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefDictionaryValue> dict) {
+	fmt::print("[R] OnBrowserCreated for browser {}\n", browser->GetIdentifier());
 	if (dict && dict->HasKey("BoltAppUrl")) {
 		this->apps.push_back(new Browser::AppFrameData(browser->GetIdentifier(), dict->GetString("BoltAppUrl")));
 	}
@@ -27,10 +28,12 @@ void Browser::App::OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<Cef
 	for (int i = 0; i < this->apps.size(); i += 1) {
 		if (this->apps[i]->id == browser->GetIdentifier()) {
 			if (this->apps[i]->url == frame->GetURL()) {
+				fmt::print("[R] OnContextCreated for app view for browser {}\n", browser->GetIdentifier());
 				CefRefPtr<CefV8Value> alt1 = CefV8Value::CreateObject(nullptr, nullptr);
 				alt1->SetValue("identifyAppUrl", CefV8Value::CreateFunction("identifyAppUrl", this->apps[i]), V8_PROPERTY_ATTRIBUTE_READONLY);
 				context->GetGlobal()->SetValue("alt1", alt1, V8_PROPERTY_ATTRIBUTE_READONLY);
 			} else if (frame->IsMain()) {
+				fmt::print("[R] OnContextCreated for app overlay for browser {}\n", browser->GetIdentifier());
 				this->apps[i]->frame = frame;
 				context->GetGlobal()->SetValue("__bolt_app_minify", CefV8Value::CreateFunction("__bolt_app_minify", this->apps[i]), V8_PROPERTY_ATTRIBUTE_READONLY);
 				context->GetGlobal()->SetValue("__bolt_app_settings", CefV8Value::CreateFunction("__bolt_app_settings", this->apps[i]), V8_PROPERTY_ATTRIBUTE_READONLY);
@@ -47,15 +50,15 @@ void Browser::App::OnUncaughtException(
 	CefRefPtr<CefV8Exception> exception,
 	CefRefPtr<CefV8StackTrace> trace
 ) {
-	fmt::print("Unhandled exception in {}: {}\n", exception->GetScriptResourceName().ToString(), exception->GetMessage().ToString());
+	fmt::print("[R] unhandled exception in {}: {}\n", exception->GetScriptResourceName().ToString(), exception->GetMessage().ToString());
 	for (int i = 0; i < trace->GetFrameCount(); i += 1) {
 		CefRefPtr<CefV8StackFrame> frame = trace->GetFrame(i);
-		fmt::print("In {}:{}:{}\n", frame->GetScriptNameOrSourceURL().ToString(), frame->GetLineNumber(), frame->GetColumn());
+		fmt::print("[R] in {}:{}:{}\n", frame->GetScriptNameOrSourceURL().ToString(), frame->GetLineNumber(), frame->GetColumn());
 	}
 	CefString source_line_ = exception->GetSourceLine();
 	if (source_line_.size() <= 180) {
 		std::string source_line = source_line_.ToString();
-		fmt::print("{}\n", source_line);
+		fmt::print("[R] {}\n[R]", source_line);
 		int i = 0;
 		while (i < exception->GetStartColumn()) {
 			fmt::print("-");
@@ -65,13 +68,15 @@ void Browser::App::OnUncaughtException(
 			fmt::print("^");
 			i += 1;
 		}
+		fmt::print("\n");
 	} else {
-		fmt::print("<origin code not shown as it is too long ({} chars)>\n", source_line_.size());
+		fmt::print("[R] <origin code not shown as it is too long ({} chars)>\n", source_line_.size());
 	}
 }
 
 bool Browser::App::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame>, CefProcessId, CefRefPtr<CefProcessMessage> message) {
 	if (message->GetName() == "__bolt_closing") {
+		fmt::print("[R] bolt_closing received for browser {}\n", browser->GetIdentifier());
 		this->apps.erase(
 			std::remove_if(this->apps.begin(), this->apps.end(), [&browser](const CefRefPtr<Browser::AppFrameData>& data){ return browser->GetIdentifier() == data->id; })
 		);
@@ -84,6 +89,7 @@ bool Browser::App::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser, CefRe
 void Browser::App::OnLoadEnd(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, int) {
 	if (CefCurrentlyOn(TID_RENDERER)) {
 		if (frame->IsMain()) {
+			fmt::print("[R] OnLoadEnd for browser {}\n", browser->GetIdentifier());
 			for(int i = 0; i < this->apps.size(); i += 1) {
 				if (this->apps[i]->id == browser->GetIdentifier()) {
 					frame->VisitDOM(new DOMVisitorAppFrame(this->apps[i]->url));
