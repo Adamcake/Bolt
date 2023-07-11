3447026a9ea111e8a22722717c188b4fa16f1682
Adam <classygopher@gmail.com>
2023-07-12 00:38:35 +0100

    main: refactor + add wWinMain for windows support

--- src/main.cxx
+++ src/main.cxx
@@ -4,6 +4,10 @@
 #include "browser/app.hxx"
 #include "browser/client.hxx"
 
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 #if defined(CEF_X11)
 #include <X11/Xlib.h>
 
@@ -24,20 +28,9 @@ int XIOErrorHandlerImpl(Display* display) {
 }
 #endif
 
-int main(int argc, char* argv[]) {
-	// Provide CEF with command-line arguments
-	// Add a flag to disable web security, because a certain company's API endpoints don't have correct
-	// access control settings; remove this setting if they ever get their stuff together
-	const char* arg = "--disable-web-security";
-	const size_t arg_len = strlen(arg);
-	char* arg_ = new char[arg_len + 1];
-	memcpy(arg_, arg, arg_len);
-	arg_[arg_len] = '\0';
-	char** argv_ = new char*[argc + 1];
-	memcpy(argv_, argv, argc * sizeof(char*));
-	argv_[argc] = arg_;
-	CefMainArgs main_args(argc + 1, argv_);
+int BoltRunBrowserProcess(CefMainArgs, CefRefPtr<Browser::App>);
 
+int BoltRunAnyProcess(CefMainArgs main_args) {
 	// CefApp struct - this implements handlers used by multiple processes
 	Browser::App cef_app_;
 	CefRefPtr<Browser::App> cef_app = &cef_app_;
@@ -49,15 +42,13 @@ int main(int argc, char* argv[]) {
 		return exit_code;
 	}
 
+	return BoltRunBrowserProcess(main_args, cef_app);
+}
+
+int BoltRunBrowserProcess(CefMainArgs main_args, CefRefPtr<Browser::App> cef_app) {
 	// CefClient struct - central object for main thread, and implements lots of handlers for browser process
 	Browser::Client client_(cef_app);
 	CefRefPtr<Browser::Client> client = &client_;
-
-#if defined(CEF_X11)
-	// X11 error handlers
-	XSetErrorHandler(XErrorHandlerImpl);
-	XSetIOErrorHandler(XIOErrorHandlerImpl);
-#endif
 
 	// CEF settings - only set the ones we're interested in
 	CefSettings settings = CefSettings();
@@ -66,7 +57,7 @@ int main(int argc, char* argv[]) {
 	settings.uncaught_exception_stack_size = 8;  // Number of call stack frames given in unhandled exception events
 
 	// Initialize CEF
-	exit_code = CefInitialize(main_args, settings, cef_app, nullptr);
+	int exit_code = CefInitialize(main_args, settings, cef_app, nullptr);
 	if (exit_code == 0) {
 		fmt::print("Exiting with error: CefInitialize exit_code {}\n", exit_code);
 		return exit_code;
@@ -74,10 +65,90 @@ int main(int argc, char* argv[]) {
 
 	// Block on the CEF message loop until CefQuitMessageLoop() is called
 	CefRunMessageLoop();
+	
+	return 0;
+}
 
-	// Shutdown and return
+#if defined(__linux__)
+int main(int argc, char* argv[]) {
+#if defined(CEF_X11)
+	// X11 error handlers
+	XSetErrorHandler(XErrorHandlerImpl);
+	XSetIOErrorHandler(XIOErrorHandlerImpl);
+#endif
+
+	// Provide CEF with command-line arguments
+	// Add a flag to disable web security, because a certain company's API endpoints don't have correct
+	// access control settings; remove this setting if they ever get their stuff together
+	const char* arg = "--disable-web-security";
+	const size_t arg_len = strlen(arg);
+	char* arg_ = new char[arg_len + 1];
+	memcpy(arg_, arg, arg_len);
+	arg_[arg_len] = '\0';
+	char** argv_ = new char*[argc + 1];
+	memcpy(argv_, argv, argc * sizeof(char*));
+	argv_[argc] = arg_;
+
+	CefMainArgs main_args(argc + 1, argv_);
+	int ret = BoltRunAnyProcess(main_args);
+
 	CefShutdown();
 	delete[] argv_;
 	delete[] arg_;
-	return 0;
+	return ret;
 }
+#endif
+
+#if defined(_WIN32)
+int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, wchar_t* lpCmdLine, int nCmdShow) {
+	// Provide CEF with command-line arguments
+	// Re-run with --disable-web-security if it's not already there, because a certain company's API endpoints
+	// don't have correct access control settings; remove this setting if they ever get their stuff together
+	int argc;
+	LPWSTR* argv = CommandLineToArgvW(lpCmdLine, &argc);
+	if (argv == nullptr) {
+		fmt::print("Exiting with error: CommandLineToArgvW failed\n");
+		return 1;
+	}
+	const wchar_t* const arg = L"--disable-web-security";
+	const wchar_t* subp_arg = L"--type=";
+	bool has_arg = false;
+	bool is_subprocess = false;
+	for(size_t i = 0; i < argc; i += 1) {
+		// check if arg is --disable-web-security
+		if (wcsstr(argv[i], arg)) {
+			has_arg = true;
+		}
+		// check if arg begins with --type=
+		if (wcsncmp(argv[i], subp_arg, wcslen(subp_arg)) == 0) {
+			is_subprocess = true;
+			break;
+		}
+	}
+	LocalFree(argv);
+
+	if (is_subprocess) {
+		int ret = BoltRunAnyProcess(CefMainArgs(hInstance));
+		CefShutdown();
+		return ret;
+	} else if (has_arg) {
+		// CefApp struct - this implements handlers used by multiple processes
+		Browser::App cef_app_;
+		CefRefPtr<Browser::App> cef_app = &cef_app_;
+		int ret = BoltRunBrowserProcess(CefMainArgs(hInstance), cef_app);
+		CefShutdown();
+		return ret;
+	} else {
+		wchar_t module_name[1024];
+		GetModuleFileNameW(NULL, module_name, 1024);
+		std::wstring command_line(lpCmdLine);
+		command_line += std::wstring(L" ");
+		command_line += std::wstring(arg);
+		PROCESS_INFORMATION process_info;
+		STARTUPINFOW startup_info;
+		GetStartupInfoW(&startup_info);
+		CreateProcessW(module_name, command_line.data(), NULL, NULL, 0, 0, NULL, NULL, &startup_info, &process_info);
+		return 0;
+	}
+}
+#endif
