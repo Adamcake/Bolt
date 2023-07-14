2dad168f5505f9b08a88785df3fc26974d3590b1
Adam <classygopher@gmail.com>
2023-07-14 05:09:00 +0100

    browser/client: handle /launch-deb

--- src/browser/client.cxx
+++ src/browser/client.cxx
@@ -1,10 +1,20 @@
 #include "client.hxx"
+#include "include/cef_parser.h"
 #include "include/cef_life_span_handler.h"
+#include "include/internal/cef_types.h"
+#include "include/internal/cef_types_wrappers.h"
+
+#if defined(__linux__)
+#include <archive.h>
+#include <archive_entry.h>
+#endif
 
 #include <algorithm>
+#include <fcntl.h>
 #include <fmt/core.h>
 #include <fstream>
 #include <regex>
+#include <spawn.h>
 
 /// https://github.com/chromiumembedded/cef/blob/5735/include/cef_resource_request_handler.h
 /// https://github.com/chromiumembedded/cef/blob/5735/include/cef_resource_handler.h
@@ -218,8 +228,13 @@ CefRefPtr<CefResourceRequestHandler> Browser::Client::GetResourceRequestHandler(
 	const CefString& request_initiator,
 	bool& disable_default_handling
 ) {
-	// One of the dreaded phrases that I don't want to show up in a grep/search
+	// Some of the dreaded phrases that I don't want to show up in a grep/search
 	constexpr char provider[] = {106, 97, 103, 101, 120, 0};
+	constexpr char tar_xz_inner_path[] = {
+		46, 47, 117, 115, 114, 47, 115, 104, 97, 114, 101, 47, 103, 97, 109, 101,
+		115, 47, 114, 117, 110, 101, 115, 99, 97, 112, 101, 45, 108, 97, 117,
+		110, 99, 104, 101, 114, 47, 114, 117, 110, 101, 115, 99,97, 112, 101
+	};
 
 	// Find the Window responsible for this request, if any
 	this->windows_lock.lock();
@@ -315,6 +330,198 @@ CefRefPtr<CefResourceRequestHandler> Browser::Client::GetResourceRequestHandler(
 		if (domain == "bolt-internal") {
 			disable_default_handling = true;
 
+#if defined(__linux__)
+			// instruction to launch RS3 .deb
+			if (path == "/launch-deb") {
+				CefRefPtr<CefPostData> post_data = request->GetPostData();
+				std::filesystem::path path = this->config_dir;
+				path.append("rs3linux");
+
+				auto cursor = 0;
+				bool has_hash = false;
+				bool has_access_token = false;
+				bool has_refresh_token = false;
+				bool has_session_id = false;
+				bool has_character_id = false;
+				bool has_display_name = false;
+				CefString hash;
+				CefString access_token;
+				CefString refresh_token;
+				CefString session_id;
+				CefString character_id;
+				CefString display_name;
+					
+				while (true) {
+					const std::string::size_type next_eq = query.find_first_of('=', cursor);
+					const std::string::size_type next_amp = query.find_first_of('&', cursor);
+					if (next_eq == std::string::npos) break;
+					if (next_eq >= next_amp) {
+						cursor = next_amp + 1;
+						continue;
+					}
+					std::string_view key(query.begin() + cursor, query.begin() + next_eq);
+					if (key == "hash") {
+						auto value_end = next_amp == std::string::npos ? query.end() : query.begin() + next_amp;
+						CefString value = std::string(std::string_view(query.begin() + next_eq + 1,  value_end));
+						access_token = CefURIDecode(value, true, UU_SPACES);
+						has_hash = true;
+					}
+					if (key == "jx_access_token") {
+						auto value_end = next_amp == std::string::npos ? query.end() : query.begin() + next_amp;
+						CefString value = std::string(std::string_view(query.begin() + next_eq + 1,  value_end));
+						access_token = CefURIDecode(value, true, UU_SPACES);
+						has_access_token = true;
+					}
+					if (key == "jx_refresh_token") {
+						auto value_end = next_amp == std::string::npos ? query.end() : query.begin() + next_amp;
+						CefString value = std::string(std::string_view(query.begin() + next_eq + 1,  value_end));
+						access_token = CefURIDecode(value, true, UU_SPACES);
+						has_refresh_token = true;
+					}
+					if (key == "jx_session_id") {
+						auto value_end = next_amp == std::string::npos ? query.end() : query.begin() + next_amp;
+						CefString value = std::string(std::string_view(query.begin() + next_eq + 1,  value_end));
+						access_token = CefURIDecode(value, true, UU_SPACES);
+						has_session_id = true;
+					}
+					if (key == "jx_character_id") {
+						auto value_end = next_amp == std::string::npos ? query.end() : query.begin() + next_amp;
+						CefString value = std::string(std::string_view(query.begin() + next_eq + 1,  value_end));
+						access_token = CefURIDecode(value, true, UU_SPACES);
+						has_character_id = true;
+					}
+					if (key == "jx_display_name") {
+						auto value_end = next_amp == std::string::npos ? query.end() : query.begin() + next_amp;
+						CefString value = std::string(std::string_view(query.begin() + next_eq + 1,  value_end));
+						access_token = CefURIDecode(value, true, UU_SPACES);
+						has_display_name = true;
+					}
+
+					if (next_amp == std::string::npos) break;
+					cursor = next_amp + 1;
+				}
+
+				if (has_hash) {
+					CefRefPtr<CefPostData> post_data = request->GetPostData();
+					if (post_data == nullptr || post_data->GetElementCount() != 1) {
+						const char* data = "Bad Request";
+						return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 400, "text/plain");
+					}
+					CefPostData::ElementVector vec;
+					post_data->GetElements(vec);
+					size_t deb_size = vec[0]->GetBytesCount();
+					unsigned char* deb = new unsigned char[deb_size];
+					vec[0]->GetBytes(deb_size, deb);
+					struct archive* ar = archive_read_new();
+					archive_read_support_format_ar(ar);
+					archive_read_open_memory(ar, deb, deb_size);
+					bool entry_found = false;
+					struct archive_entry* entry;
+					while (true) {
+						int r = archive_read_next_header(ar, &entry);
+						if (r == ARCHIVE_EOF) break;
+						if (r != ARCHIVE_OK) {
+							archive_read_close(ar);
+							archive_read_free(ar);
+							delete[] deb;
+							const char* data = "Malformed .deb file\n";
+							return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 400, "text/plain");
+						}
+						if (strcmp(archive_entry_pathname(entry), "data.tar.xz") == 0) {
+							entry_found = true;
+							break;
+						}
+					}
+					if (!entry_found) {
+						archive_read_close(ar);
+						archive_read_free(ar);
+						delete[] deb;
+						const char* data = "No data in .deb file\n";
+						return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 400, "text/plain");
+					}
+
+					const long entry_size = archive_entry_size(entry);
+					unsigned char* tar_xz = new unsigned char[entry_size];
+					long written = 0;
+					while (written < entry_size) {
+						written += archive_read_data(ar, tar_xz + written, entry_size - written);
+					}
+					archive_read_close(ar);
+					archive_read_free(ar);
+					delete[] deb;
+
+					struct archive* xz = archive_read_new();
+					archive_read_support_format_tar(xz);
+					archive_read_support_filter_xz(xz);
+					archive_read_open_memory(xz, tar_xz, entry_size);
+					entry_found = false;
+					while (true) {
+						int r = archive_read_next_header(xz, &entry);
+						if (r == ARCHIVE_EOF) break;
+						if (r != ARCHIVE_OK) {
+							archive_read_close(xz);
+							archive_read_free(xz);
+							delete[] tar_xz;
+							const char* data = "Malformed .tar.xz file\n";
+							return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 400, "text/plain");
+						}
+						if (strcmp(archive_entry_pathname(entry), tar_xz_inner_path) == 0) {
+							entry_found = true;
+							break;
+						}
+					}
+					if (!entry_found) {
+						archive_read_close(xz);
+						archive_read_free(xz);
+						delete[] tar_xz;
+						const char* data = "No target executable in .tar.xz file\n";
+						return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 400, "text/plain");
+					}
+
+					const long game_size = archive_entry_size(entry);
+					char* game = new char[game_size];
+					written = 0;
+					while (written < game_size) {
+						written += archive_read_data(ar, game + written, game_size - written);
+					}
+					archive_read_close(xz);
+					archive_read_free(xz);
+					delete[] tar_xz;
+
+					written = 0;
+					int file = open(path.c_str(), O_WRONLY | O_CREAT, 0755);
+					while (written < game_size) {
+						written += write(file, game + written, game_size - written);
+					}
+					close(file);
+					delete[] game;
+				}
+
+				posix_spawn_file_actions_t file_actions;
+				posix_spawnattr_t attributes;
+				pid_t pid;
+				posix_spawn_file_actions_init(&file_actions);
+				posix_spawnattr_init(&attributes);
+				std::string path_str(path.c_str());
+				char* argv[2];
+				argv[0] = path_str.data();
+				argv[1] = nullptr;
+				char* env[8];
+				memset(env, 0, sizeof env);
+				int r = posix_spawn(&pid, path_str.c_str(), &file_actions, &attributes, argv, env);
+
+				if (r == 0) {
+					fmt::print("Successfully spawned game process with pid {}\n", pid);
+					const char* data = "OK\n";
+					return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 200, "text/plain");
+				} else {
+					fmt::print("[B] Error from posix_spawn: {}\n", r);
+					const char* data = "Error spawning process\n";
+					return new ResourceHandler(reinterpret_cast<const unsigned char*>(data), strlen(data), 500, "text/plain");
+				}
+			}
+#endif
+
 			// respond using internal hashmap of filenames
 			auto it = std::find_if(
 				this->internal_pages.begin(),
@@ -341,7 +548,7 @@ CefRefPtr<CefResourceRequestHandler> Browser::Client::GetResourceRequestHandler(
 			}
 		}
 	}
-	
+
 	// route the request normally, to a website or whatever
 	return nullptr;
 }
