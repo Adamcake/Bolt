b78e65f06886ef7fc14394294530ff136fdca7d8
Adam <classygopher@gmail.com>
2023-08-25 21:31:03 +0100

    browser/app: default_config_uri

--- src/browser/app.cxx
+++ src/browser/app.cxx
@@ -112,7 +112,8 @@ bool Browser::App::Execute(const CefString&, CefRefPtr<CefV8Value>, const CefV8V
 	retval->SetValue("profile_api", CefV8Value::CreateString("aHR0cHM6Ly9zZWN1cmUuamFnZXguY29tL3JzLXByb2ZpbGUvdjE"), V8_PROPERTY_ATTRIBUTE_READONLY);
 	retval->SetValue("shield_url", CefV8Value::CreateString("aHR0cHM6Ly9hdXRoLmphZ2V4LmNvbS9zaGllbGQvb2F1dGgvdG9rZW4"), V8_PROPERTY_ATTRIBUTE_READONLY);
 	retval->SetValue("content_url", CefV8Value::CreateString("aHR0cHM6Ly9jb250ZW50LnJ1bmVzY2FwZS5jb20vZG93bmxvYWRzL3VidW50dS8"), V8_PROPERTY_ATTRIBUTE_READONLY);
-	
+	retval->SetValue("default_config_uri", CefV8Value::CreateString("aHR0cHM6Ly93d3cucnVuZXNjYXBlLmNvbS9rPTUvbD0wL2phdl9jb25maWcud3M"), V8_PROPERTY_ATTRIBUTE_READONLY);
+
 	CefRefPtr<CefV8Value> games = CefV8Value::CreateArray(2);
 	games->SetValue(0, CefV8Value::CreateString("UnVuZVNjYXBl"));
 	games->SetValue(1, CefV8Value::CreateString("T2xkIFNjaG9vbA"));
