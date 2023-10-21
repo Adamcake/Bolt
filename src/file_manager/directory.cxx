5ae8216637c38d8217f175ca879225884d12a8e9
Adam <classygopher@gmail.com>
2023-10-21 03:51:03 +0100

    file_manager/directory: remove debug prints

--- src/file_manager/directory.cxx
+++ src/file_manager/directory.cxx
@@ -3,13 +3,11 @@
 #include "include/cef_parser.h"
 
 #include <fstream>
-#include <iostream>
 
 FileManager::Directory::Directory(std::filesystem::path path): path(path) {}
 
 FileManager::File FileManager::Directory::get(std::string_view uri) const {
 	std::filesystem::path path = std::string(this->path) + std::string(uri);
-	std::cout << "[B] serving " << path.c_str() << std::endl;
 	std::ifstream file(path, std::ios::in | std::ios::binary);
     if (file.fail()) return File { .contents = nullptr, .size = 0 };
     file.seekg(0, std::ios::end);
