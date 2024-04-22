b958ec642ec5ff329776cf684c923b68985b6b1b
Adam <classygopher@gmail.com>
2024-04-23 00:59:07 +0100

    resource_handler: std::string constructors
    Constructors that accept "owned data" to handle situations where origin
    data may be going out of scope

--- src/browser/resource_handler.hxx
+++ src/browser/resource_handler.hxx
@@ -5,6 +5,7 @@
 
 #include "include/cef_base.h"
 #include "include/cef_resource_request_handler.h"
+#include <string>
 
 namespace Browser {
 	/// Struct for sending some bytes from memory as an HTTP response. Store individual instances on the heap.
@@ -15,6 +16,15 @@ namespace Browser {
 			data(data), data_len(len), status(status), mime(mime), has_location(false), cursor(0), file_manager(nullptr) { }
 		ResourceHandler(const unsigned char* data, size_t len, int status, const char* mime, CefString location):
 			data(data), data_len(len), status(status), mime(mime), location(location), has_location(true), cursor(0), file_manager(nullptr) { }
+
+		ResourceHandler(std::string data, int status, const char* mime):
+			internal_string(data), data_len(data.length()), status(status), mime(mime), has_location(false), cursor(0), file_manager(nullptr) {
+			this->data = reinterpret_cast<const unsigned char*>(this->internal_string.c_str());
+		}
+		ResourceHandler(std::string data, int status, const char* mime, CefString location):
+			internal_string(data), data_len(data.length()), status(status), mime(mime), location(location), has_location(true), cursor(0), file_manager(nullptr) {
+			this->data = reinterpret_cast<const unsigned char*>(this->internal_string.c_str());
+		}
 		
 		/// This constructor assumes the file does exist i.e. all params are initialised, and status will be 200
 		ResourceHandler(FileManager::File file, CefRefPtr<FileManager::FileManager> file_manager):
@@ -39,6 +49,7 @@ namespace Browser {
 			CefString location;
 			bool has_location;
 			size_t cursor;
+			const std::string internal_string;
 			CefRefPtr<FileManager::FileManager> file_manager;
 			IMPLEMENT_REFCOUNTING(ResourceHandler);
 			DISALLOW_COPY_AND_ASSIGN(ResourceHandler);
