fcda68c9665213e101667d4ed293e1fcdee644d0
Kmc <kmcgurty1@gmail.com>
2024-06-14 00:11:52 -0400

    app: add source map if in dev

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,14 +4,19 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	build: {
-		outDir: 'dist'
-	},
-	plugins: [tsconfigPaths(), svelte()],
-	css: {
-		postcss: {
-			plugins: [tailwindcss()]
+
+export default defineConfig(({ mode }) => {
+	const dev = mode === 'development';
+	return {
+		build: {
+			sourcemap: dev ? 'inline' : false,
+			outDir: 'dist'
+		},
+		plugins: [tsconfigPaths(), svelte()],
+		css: {
+			postcss: {
+				plugins: [tailwindcss()]
+			}
 		}
-	}
+	};
 });
