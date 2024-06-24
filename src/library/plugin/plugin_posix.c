c8ae806402243104ad2fdfb0afdb4054715e3313
Adam <classygopher@gmail.com>
2024-06-24 22:18:46 +0100

    library: add all missing win32 files and functions

--- src/library/plugin/plugin_posix.c
+++ src/library/plugin/plugin_posix.c
@@ -1,3 +1,5 @@
+#include "plugin.h"
+
 #include <errno.h>
 #include <poll.h>
 #include <stdlib.h>
---
+++ src/library/plugin/plugin_win32.c
@@ -0,0 +1,9 @@
+#include "plugin.h"
+
+void _bolt_plugin_ipc_init(int* fd) {
+    // TODO
+}
+
+void _bolt_plugin_ipc_close(int fd) {
+    // TODO
+}
