f17642e1429e64352e3ff62a010543ef945aad4e
Kmc <kmcgurty1@gmail.com>
2024-07-05 13:43:52 -0400

    app: add CookieService and LocalStorageService

---
+++ app/src/lib/Enums/Time.ts
@@ -0,0 +1,9 @@
+export enum Time {
+	SECOND = 1000,
+	MINUTE = 60 * 1000,
+	HALF_HOUR = 30 * 60 * 1000,
+	HOUR = 60 * 60 * 1000,
+	DAY = 24 * 60 * 60 * 1000,
+	WEEK = 7 * 24 * 60 * 60 * 1000,
+	MONTH = 30 * 24 * 60 * 60 * 1000
+}
