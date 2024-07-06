0b2aecfbaed5ee547fa2cc5f7e5978d64056a5fc
Adam <classygopher@gmail.com>
2024-07-06 04:10:28 +0100

    window_launcher: launch exe on windows

--- src/browser/window_launcher.cxx
+++ src/browser/window_launcher.cxx
@@ -555,6 +555,26 @@ void Browser::Launcher::Refresh() const {
 	this->browser->GetMainFrame()->LoadURL(this->BuildURL());
 }
 
+void Browser::Launcher::ParseQuery(std::string_view query, std::function<void(const std::string_view&, const std::string_view&)> callback) {
+	size_t pos = 0;
+	while (true) {
+		const size_t next_and = query.find('&', pos);
+		const size_t next_eq = query.find('=', pos);
+		if (next_eq == std::string_view::npos) break;
+		else if (next_and != std::string_view::npos && next_eq > next_and) {
+			pos = next_and + 1;
+			continue;
+		}
+		const bool is_last = next_and == std::string_view::npos;
+		const auto end = is_last ? query.end() : query.begin() + next_and;
+		const std::string_view key(query.begin() + pos, query.begin() + next_eq);
+		const std::string_view val(query.begin() + next_eq + 1, end);
+		callback(key, val);
+		if (is_last) break;
+		pos = next_and + 1;
+	}
+}
+
 CefRefPtr<CefResourceRequestHandler> SaveFileFromPost(CefRefPtr<CefRequest> request, const std::filesystem::path::value_type* path) {
 	CefRefPtr<CefPostData> post_data = request->GetPostData();
 	if (post_data->GetElementCount() != 1) {
