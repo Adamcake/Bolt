c37d0a55bebf8b6d166241617063e5bbc2d32f13
Adam <classygopher@gmail.com>
2024-07-26 17:57:03 +0100

    library: identify entrypoint by ordinal
    With an on-the-fly .def file, I can force a function to be exported with
    ordinal 1, meaning I know its offset into the table in the .dll without
    having to parse anything.

--- src/library/dll/stub.c
+++ src/library/dll/stub.c
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <stdio.h>
 
-int main() {
+// this function is invoked via CreateRemoteThread by the manual map injector (see stub_inject)
+// after it injects it into the process.
+void BOLT_STUB_ENTRYNAME(void* data) {
     printf("stub hello world\n");
 }
