6b102466296568e1ef53c51e623e7678f0783b6c
Adam <classygopher@gmail.com>
2024-08-12 21:35:30 +0100

    library: use correct ipc path for windows

--- src/library/plugin/plugin_posix.c
+++ src/library/plugin/plugin_posix.c
@@ -23,11 +23,17 @@ void _bolt_plugin_ipc_init(BoltSocketType* fd) {
     const int olderr = errno;
     struct sockaddr_un addr = {.sun_family = AF_UNIX};
     *fd = socket(addr.sun_family, SOCK_STREAM, 0);
+#if defined(_WIN32)
+    const char* runtime_dir = getenv("appdata");
+    snprintf(addr.sun_path, sizeof(addr.sun_path) - 1, "%s\\bolt-launcher\\run\\ipc-0", runtime_dir);
+#else
     const char* runtime_dir = getenv("XDG_RUNTIME_DIR");
     const char* prefix = (runtime_dir && *runtime_dir) ? runtime_dir : "/tmp";
     snprintf(addr.sun_path, sizeof(addr.sun_path) - 1, "%s/bolt-launcher/ipc-0", prefix);
+#endif
     if (connect(*fd, (const struct sockaddr*)&addr, sizeof(addr)) == -1) {
         printf("error: IPC connect() error %i\n", errno);
+        exit(1);
     }
     errno = olderr;
 }
